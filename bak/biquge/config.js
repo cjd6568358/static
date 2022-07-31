8ef35cbd7fa628f7ebba7968c93c8381ca1e567e
cjd6568358 <cjd6568358@163.com>
2022-08-01 00:52:27 +0800

    fix

--- bak/biquge/config.js
+++
@@ -1,6 +0,0 @@
-var globalConfig = {
-    "baseUrl": "https://cjd6568358.3322.org:6706/api/",
-    "configUrl": "https://cjd6568358.3322.org:6706/static/biquge/config.json",
-    "soterAuthCheck": false,
-    "testing": false,
-}
